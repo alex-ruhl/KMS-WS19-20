6767778fe34991f4a29a2ad548b25aedb6153961
Andre Lammers <andre.lammers@mni.thm.de>
2019-10-26 14:39:15 +0200

    Update 189.c (only codestyle)

--- 189.c
+++ 189.c
@@ -3,7 +3,7 @@ void rotate(int* nums, int numsSize, int k){
     int j;
     int lastElement;
     lastElement = nums[numsSize - 1];
-    for(j=numsSize-1; j>0; j--){
+    for(j = numsSize - 1; j > 0; j--){
        nums[j] = nums[j - 1];
     }
     nums[0] = lastElement;
